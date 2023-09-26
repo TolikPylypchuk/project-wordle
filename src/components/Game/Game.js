114f3c407b62792a04efae3164c5809748a87a34
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2023-09-26 23:25:27 +0300

    Add the solution for exercise 2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GuessInput from "../GuessInput";
+import GuessList from "../GuessList";
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
@@ -10,9 +11,17 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+
   return (
     <>
-      <GuessInput />
+      <GuessList guesses={guesses} />
+      <GuessInput
+        onSubmit={(guess) => {
+          const nextGuess = { word: guess, id: Math.random() };
+          setGuesses([...guesses, nextGuess]);
+        }}
+      />
     </>
   );
 }
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,27 +1,29 @@
 import React from "react";
 
-function GuessInput() {
+function GuessInput({ onSubmit }) {
   const wordLength = 5;
 
   const [guess, setGuess] = React.useState("");
 
-  function onSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
 
     if (guess.length === wordLength) {
       console.log(guess);
+      onSubmit(guess);
       setGuess("");
     }
   }
 
   return (
-    <form onSubmit={onSubmit} className="guess-input-wrapper">
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
         minLength={wordLength}
         maxLength={wordLength}
+        pattern="[a-zA-Z]{5}"
         value={guess}
         onChange={(event) => setGuess(event.target.value.toUpperCase())}
       />
